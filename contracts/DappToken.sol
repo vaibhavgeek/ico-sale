a1c219b9512d38d76db7e71320776fb008f3d2ba
Vaibhav Maheshwari <vaibhavgeek@pop-os.localdomain>
2021-07-23 23:05:17 +0530

    Added tests for symbol and token along with solidity contract

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -2,11 +2,17 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract DappToken {
+    string public name = "VAIBHAV";
+    string public symbol = "VAI";
   // constructor 
   // set the total no of tokens
   // read the total number of tokens
   uint256 public totalSupply;
-  constructor() public {
-      totalSupply = 1000000;
+  mapping (address => uint256) public balanceOf;
+  constructor(uint256 _initialSupply) public {
+    // allocate the initial supply
+    balanceOf[msg.sender] = _initialSupply;
+    // totalsupply is initial supply 
+    totalSupply = _initialSupply;
   }
 }
--- test/DappToken.js
+++ test/DappToken.js
@@ -1,12 +1,29 @@
 var DappToken = artifacts.require("./DappToken.sol")
 
 contract('DappToken', function(accounts) {
+    var tokenInstance;
+
+    it("initializes contract with correct values", function(){
+            return DappToken.deployed().then(async function (instance){
+            tokenInstance = instance;
+            return await tokenInstance.name();
+        }).then(function(name){
+            assert.equal(name, "VAIBHAV", "has the correct name");
+            return tokenInstance.symbol();
+        }).then(function(symbol){
+            assert.equal(symbol, "VAI", "has the correct symbol");
+        });
+    });
+
     it("sets the total supply upon deployment", function(){
         return DappToken.deployed().then(async function (instance){
             tokenInstance = instance;
             return await tokenInstance.totalSupply();
         }).then(function(totalSupply){
             assert.equal(totalSupply.toNumber(), 1000000, 'sets tht total supply to 1000000 (1 million)');
+            return tokenInstance.balanceOf(accounts[0]);
+        }).then(function(adminBalance){
+                assert.equal(adminBalance.toNumber(), 1000000, "admin check initial supply");
         })
     });
 })
